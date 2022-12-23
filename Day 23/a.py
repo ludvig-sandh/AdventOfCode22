9bc476cabefb33e579d850dbbefdcb40e70816f2
ludvig-sandh <givdul11@hotmail.se>
2022-12-23 14:19:19 +0100

    Day 23 complete

---
+++ Day 23/a.py
@@ -0,0 +1,82 @@
+elves = set()
+with open("in.txt", "r") as f:
+    for r, line in enumerate(f.read().split("\n")):
+        for c in range(len(line)):
+            if line[c] == '#':
+                elves.add((r, c))
+
+order = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+def check_direction(direction, r, c):
+    dr, dc = direction
+    if dr == 0:
+        for nr in range(r - 1, r + 2):
+            if (nr, c + dc) in elves:
+                return False
+    elif dc == 0:
+        for nc in range(c - 1, c + 2):
+            if (r + dr, nc) in elves:
+                return False
+    return True
+
+def count_ground():
+    ir, ic = 1000000, 1000000
+    ar, ac = -1000000, -1000000
+    for (r, c) in elves:
+        ir = min(ir, r)
+        ic = min(ic, c)
+        ar = max(ar, r)
+        ac = max(ac, c)
+    ans = 0
+    for r in range(ir, ar + 1):
+        for c in range(ic, ac + 1):
+            if (r, c) not in elves:
+                ans += 1
+    return ans
+
+def display():
+    ir, ic = 1000000, 1000000
+    ar, ac = -1000000, -1000000
+    for (r, c) in elves:
+        ir = min(ir, r)
+        ic = min(ic, c)
+        ar = max(ar, r)
+        ac = max(ac, c)
+    ir, ic = 0, 0
+    ar, ac = 5, 5
+    for r in range(ir, ar + 1):
+        for c in range(ic, ac + 1):
+            if (r, c) in elves:
+                print("#", end="")
+            else:
+                print(".", end="")
+        print()
+    print()
+
+NUM_ROUNDS = 10
+
+for current_round in range(NUM_ROUNDS):
+    wanted_moves = dict()
+    for (r, c) in elves:
+        no_elves = True
+        for direction in order:
+            if not check_direction(direction, r, c):
+                no_elves = False
+                break
+        if no_elves:
+            continue
+        for direction in order:
+            if check_direction(direction, r, c):
+                key = (r + direction[0], c + direction[1])
+                if key not in wanted_moves:
+                    wanted_moves[key] = []
+                wanted_moves[key].append((r, c))
+                break
+    
+    for wanted_move, wanted_elves in wanted_moves.items():
+        if len(wanted_elves) == 1:
+            elves.remove(wanted_elves[0])
+            elves.add(wanted_move)
+    order = order[1:] + [order[0]]
+    
+print(count_ground())
